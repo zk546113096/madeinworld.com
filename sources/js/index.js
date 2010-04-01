ed305154fbee60bf43af11082afbb1e396dec9da
zk <zk546113096@gmail.com>
2010-04-01 15:45:25 +0800

    首页修改完成。。。

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
-  $('#coin-slider').coinslider({ 
-    width: 1000, 
-    height: 327,
-    navigation: false,
-    delay: 5000,
-    navigation: true,
-    links: false,
-    sDelay: 30, // delay beetwen squares in ms
-    titleSpeed: 500, // speed of title appereance in ms
-    hoverPause: true // pause on hover
-  });
-  $('.cs-buttons').hide();
+  $('.factory-search-text').toggleInputText();
+
+  $(".carousel").jCarouselLite({/* 商品切换列表 前翻后翻 {{{ */
+    visible: 1,
+    btnNext: ".next",
+    btnPrev: ".prev"
+  });/* }}} */
 });
