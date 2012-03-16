f6fddc901bf732bda91d8ffd06690d33166c55cf
zk <zk546113096@gmail.com>
2012-03-16 09:05:43 +0800

    b2b视图基本搭建，loggoss资源页面完成

---
+++ sources/js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $.fn.toggleInputText = function() { /* toggle input {{{ */
+    var v = $(this).val(); 
+    if($(this).val() == "")
+      $(this).val(v).css({'color' : '#ccc'});
+    $(this).focus(function(){
+      $(this).val('').select().css({'color' : '#000'});
+    }).blur(function(){
+      if($(this).val()==""){
+         $(this).val(v).css({'color' : '#ccc'});
+      }
+    });
+  };/* }}} */
+  $('.switch-tabs > li').mouseover(function() {/* tab 鼠标经过切换 切换 {{{ */
+    $(this).addClass('current').siblings().removeClass('current');
+
+    var idx = $('.switch-tabs > li').index(this);
+    $('.switch-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
+    return false;
+  });/* }}} */
+});
---
+++ sources/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.top-menu-text').toggleInputText();
+  $('.header-search-text').toggleInputText();
+
+  $('.header-search-tabs > li').click(function() {/* 首页 搜索框 切换 {{{ */
+    $(this).addClass('current').siblings().removeClass('current');
+
+    var idx = $('.header-search-tabs > li').index(this);
+    $(this).parents('#header-search').css(
+      'background', 'url("./sources/images/base/header-search-' + idx + '.png") no-repeat' 
+    );
+    $('.header-search-blocks > li:eq(' + idx + ')').addClass('current').siblings().removeClass('current');
+    return false;
+  });/* }}} */
+
+});
