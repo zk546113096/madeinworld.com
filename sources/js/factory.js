084955c68293bf2d53a70ea017650274c24af898
zk <zk546113096@gmail.com>
2012-03-31 16:18:52 +0800

    行业厂家 列表页完成

--- sources/js/factory.js
+++ sources/js/factory.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   $('.factory-search-text').toggleInputText();
+
+  $('.list-sidebar ul li').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+
+  $('.list-cont-list dd').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
 });
